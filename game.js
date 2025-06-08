02a171b1a791c85c939392f39a7a84c6501a5c1f
willyoubemyfriend <chiyaburro@gmail.com>
2025-06-08 19:04:23 -0400

    Update game.js

--- game.js
+++ game.js
@@ -238,7 +238,7 @@ function drawInventoryPage1() {
 
     // HP & Location under portrait
     ctx.fillText(`HP: `, 16, 118);
-    ctx.fillText(`${playerStats.name}`,16, 112);
+    ctx.fillText(`${playerStats.name}`,16, 104);
     ctx.font = '16px "friendfont"';
     ctx.fillText(`${playerStats.hp}/${playerStats.maxhp}`,48, 117);
     ctx.fillText(`${playerStats.location}`, 16, 128);
