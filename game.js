6bdeb6fc93f900de2f0bd067e06ed0b235fc293d
willyoubemyfriend <chiyaburro@gmail.com>
2025-06-08 17:39:11 -0400

    Update game.js

--- game.js
+++ game.js
@@ -28,7 +28,7 @@ const playerStats = {
     attack: 5,
     defense: 5,
     dread: 15,
-    location: "ÖCum Dungeon",
+    location: "Ö Cum Dungeon",
     description: "You need to take a shit."
 };
 
